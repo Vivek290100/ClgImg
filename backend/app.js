c21f91f5ff47788529581566f0d3ce0fe9e418c6
vivek <vivekv290100@gmail.com>
2025-11-06 19:35:06 +0530

    add test api + logs

--- backend/app.js
+++ backend/app.js
@@ -15,11 +15,21 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true}))
 app.use(cookieParser())
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173','https://img-one-lime.vercel.app/'],
     credentials: true,
 }
 
 app.use(cors(corsOptions))
+
+app.use((req, res, next) => {
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.url} from ${req.ip}`);
+  next();
+});
+
+app.get('/api/v1/test', (req, res) => {
+  console.log('TEST API HIT! 🚀');
+  res.json({ message: 'Backend woke up! Ready to rock!' });
+});
 
 
 app.use('/api/v1', userRoutes)
--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,2 +1,5 @@
-export const USER_API_ENDPOINT = "http://localhost:8000/api/v1"
-export const ADMIN_API_ENDPOINT = "http://localhost:8000/api/v1/admin"
+// export const USER_API_ENDPOINT = "http://localhost:8000/api/v1"
+// export const ADMIN_API_ENDPOINT = "http://localhost:8000/api/v1/admin"
+
+export const USER_API_ENDPOINT = "https://clg-img-api.onrender.com/api/v1"
+export const ADMIN_API_ENDPOINT = "https://clg-img-api.onrender.com/api/v1/admin"
