5ef45b03ea78ab10bce875eb1400969833cc7b23
vivek <vivekv290100@gmail.com>
2025-11-06 20:11:54 +0530

    =>

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true}))
 app.use(cookieParser())
 app.use(cors({
-  origin: true,           // ← ALLOWS EVERYTHING (perfect for now)
+  origin: true,       
   credentials: true
 }));
 
