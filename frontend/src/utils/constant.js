fa36b19350f94c68a3246d4db4327682afc406bf
vivek <vivekv290100@gmail.com>
2025-11-06 20:33:17 +0530

    =>=>=>

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,5 +1,2 @@
-// export const USER_API_ENDPOINT = "http://localhost:8000/api/v1"
-// export const ADMIN_API_ENDPOINT = "http://localhost:8000/api/v1/admin"
-
 export const USER_API_ENDPOINT = "https://clgimg.onrender.com/api/v1"
 export const ADMIN_API_ENDPOINT = "https://clgimg.onrender.com/api/v1/admin"
